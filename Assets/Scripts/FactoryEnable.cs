027e552cc10f94556039d04ca18c0557ae47856a
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2023-08-17 17:56:47 +0300

    Add FactoryEnable and FactoryPoint

--- Assets/Scripts/FactoryEnable.cs
+++ Assets/Scripts/FactoryEnable.cs
@@ -6,11 +6,37 @@ public class FactoryEnable : MonoBehaviour
 {
     [SerializeField] private Factory[] _factories;
 
+    private Queue<Factory> _factoriesQueue = new Queue<Factory>();
+    //private float _time; // for test
+
     private void Start()
     {
+        //_time = 0; // for test
+
         foreach (Factory factory in _factories)
         {
-            Debug.Log(factory.name);
+            _factoriesQueue.Enqueue(factory);
+            factory.gameObject.SetActive(false);
         }
-    } 
+    }
+
+    //private void Update() // for test
+    //{
+    //    _time += Time.deltaTime;
+
+    //    if (_time > 5f)
+    //    {
+    //        BuyFactory();
+    //        _time = 0;
+    //    }
+    //}
+
+    public void BuyFactory()
+    {
+        if (_factoriesQueue.Count > 0)
+        {
+            Factory factory = _factoriesQueue.Dequeue();
+            factory.gameObject.SetActive(true);
+        }
+    }
 }
